7051d7e4676b52befeba45450cad5c79a413f59f
djpeluca <adrianbs@gmail.com>
2023-07-29 23:33:26 -0300

    first commit

--- src/subscription.ts
+++ src/subscription.ts
@@ -4,26 +4,48 @@ import {
 } from './lexicon/types/com/atproto/sync/subscribeRepos'
 import { FirehoseSubscriptionBase, getOpsByType } from './util/subscription'
 
+//Include words to match the post and add them
+const matchText: string[] = [
+  '#Uruguay',
+  'Uruguay',
+  'Uruguai',
+  'Uruguaya',
+  'Uruguayo',
+  'Montevideo'
+]
+
+const matchPatterns: RegExp[] = [
+  /(^|[\s\W])Uruguay($|[\W\s])/im,
+  /(^|[\s\W])Montevideo($|[\W\s])/im
+]
+
+// Include Uruguayan users here to always include their posts
+const matchUsers: string[] = [
+  //
+]
+
+// Exclude posts from these users
+const bannedUsers: string[] = [
+  //
+]
+
 export class FirehoseSubscription extends FirehoseSubscriptionBase {
   async handleEvent(evt: RepoEvent) {
     if (!isCommit(evt)) return
     const ops = await getOpsByType(evt)
 
-    // This logs the text of every post off the firehose.
-    // Just for fun :)
-    // Delete before actually using
-    for (const post of ops.posts.creates) {
-      console.log(post.record.text)
-    }
-
     const postsToDelete = ops.posts.deletes.map((del) => del.uri)
     const postsToCreate = ops.posts.creates
       .filter((create) => {
-        // only alf-related posts
-        return create.record.text.toLowerCase().includes('alf')
+        const txt = create.record.text.toLowerCase()
+        return (
+          (matchText.some((term) => txt.includes(term)) ||
+            matchPatterns.some((pattern) => pattern.test(txt)) ||
+            matchUsers.includes(create.author)) &&
+          !bannedUsers.includes(create.author)
+        )
       })
       .map((create) => {
-        // map alf-related posts to a db row
         return {
           uri: create.uri,
           cid: create.cid,
